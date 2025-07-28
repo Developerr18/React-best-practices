4a6afa3d2310f8640a4d88f27698aeba81bf9584
Developerr18 <dineshnikum1@gmail.com>
2025-07-28 14:07:00 +0530

    different comps for app

--- animal-project/src/App.jsx
+++ animal-project/src/App.jsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
+function getRandomAnimal() {
+  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+
+  const randomNum = Math.floor(Math.random() * animals.length);
+
+  return animals[randomNum];
+}
+
 const App = () => {
-  return <div>App</div>;
+  const [animals, setAnimals] = useState([]);
+
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()]);
+  };
+
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow key={index} type={animal} />;
+  });
+
+  return (
+    <div>
+      <button onClick={handleClick}>Add Animal</button>
+      <div>{renderedAnimals}</div>
+    </div>
+  );
 };
 
 export default App;
