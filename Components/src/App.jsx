f0256748a6e3d223f760b1a3a8436cb5be8f2eaa
Developerr18 <dineshnikum1@gmail.com>
2025-07-30 00:37:50 +0530

    start making accordion comp app

--- Components/src/App.jsx
+++ Components/src/App.jsx
@@ -1,49 +1,33 @@
-import "./App.css";
-import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
-import Button from "./Button";
+import Accordion from "./components/Accordion";
 
-function App() {
-  return (
-    <div>
-      <div>
-        <Button
-          primary
-          onClick={() => console.log("Primary button clicked")}
-          onMouseMove={() => console.log("Primary button hover")}
-        >
-          <GoBell /> Primary
-        </Button>
-      </div>
-      <div>
-        <Button
-          secondary
-          rounded
-          onMouseEnter={() => console.log("Secondary button mouse enter")}
-        >
-          Secondary
-        </Button>
-      </div>
-      <div>
-        <Button
-          success
-          outline
-          onClick={() => console.log("Success button clicked")}
-        >
-          <GoCloudDownload /> Success
-        </Button>
-      </div>
-      <div>
-        <Button danger className="ml-7">
-          <GoDatabase /> Danger
-        </Button>
-      </div>
-      <div>
-        <Button warning rounded>
-          Warning
-        </Button>
-      </div>
-    </div>
-  );
+export default function App() {
+  const data = [
+    {
+      label: "What is React?",
+      content:
+        "React is a JavaScript library for building user interfaces. It lets you compose complex UIs from small, reusable components.",
+    },
+    {
+      label: "What is a component?",
+      content:
+        "A component is a reusable piece of UI that can be nested, managed, and handled independently in a React application.",
+    },
+    {
+      label: "What is JSX?",
+      content:
+        "JSX stands for JavaScript XML. It allows you to write HTML in React and makes the code easier to understand and debug.",
+    },
+    {
+      label: "What are props in React?",
+      content:
+        "Props are inputs to a React component. They are passed down from parent to child and are read-only.",
+    },
+    {
+      label: "What is state in React?",
+      content:
+        "State is a built-in object that stores property values that belong to the component. When the state object changes, the component re-renders.",
+    },
+  ];
+
+  return <Accordion />;
 }
-
-export default App;
---
+++ Components/src/pages/ButtonPage.jsx
@@ -0,0 +1,47 @@
+import "./App.css";
+import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
+import Button from "../components/Button";
+
+export default function ButtonPage() {
+  return (
+    <div>
+      <div>
+        <Button
+          primary
+          onClick={() => console.log("Primary button clicked")}
+          // onMouseMove={() => console.log("Primary button hover")}
+        >
+          <GoBell /> Primary
+        </Button>
+      </div>
+      <div>
+        <Button
+          secondary
+          rounded
+          // onMouseEnter={() => console.log("Secondary button mouse enter")}
+        >
+          Secondary
+        </Button>
+      </div>
+      <div>
+        <Button
+          success
+          outline
+          onClick={() => console.log("Success button clicked")}
+        >
+          <GoCloudDownload /> Success
+        </Button>
+      </div>
+      <div>
+        <Button danger className="ml-7">
+          <GoDatabase /> Danger
+        </Button>
+      </div>
+      <div>
+        <Button warning rounded>
+          Warning
+        </Button>
+      </div>
+    </div>
+  );
+}
