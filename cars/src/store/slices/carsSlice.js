3dc2a7938b83bc7188053944f9aa68753a815857
Developerr18 <dineshnikum1@gmail.com>
2025-08-03 03:17:32 +0530

    update

---
+++ cars/src/store/slices/carsSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const carsSlice = createSlice({
+  name: "cars",
+  initialState: {
+    searchTerm: "",
+    cars: [],
+  },
+  reducers: {
+    changeSearchTerm(state, action) {
+      state.searchTerm = action.payload;
+    },
+    addCar(state, action) {},
+    removeCar(state, action) {},
+  },
+});
