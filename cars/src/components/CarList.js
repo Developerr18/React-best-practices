eab860ee6121ce9abdc2b17b6712b277ac1a1a2a
Developerr18 <dineshnikum1@gmail.com>
2025-08-04 02:02:33 +0530

    completed cars project

--- cars/src/components/CarList.js
+++ cars/src/components/CarList.js
@@ -1,25 +1,34 @@
 import { useSelector, useDispatch } from "react-redux";
+import { createSelector } from "@reduxjs/toolkit";
 import { removeCar } from "../store";
 
+const memoizedCars = createSelector(
+  [(state) => state.cars.carsList, (state) => state.cars.searchTerm],
+  (data, searchTerm) =>
+    data.filter((car) =>
+      car.name.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+);
+
 const CarList = () => {
-  const cars = useSelector((state) => state.cars.carsList);
+  const cars = useSelector(memoizedCars);
+  const name = useSelector((state) => state.form.name);
   const dispatch = useDispatch();
 
   const renderedCars = cars.map((car) => {
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
     return (
-      <>
-        <div key={car.id} className="panel">
-          <p>
-            {car.name} - ${car.cost}
-          </p>
-          <button
-            className="button is-danger"
-            onClick={() => dispatch(removeCar(car.id))}
-          >
-            Delete
-          </button>
-        </div>
-      </>
+      <div key={car.id} className={`panel ${bold && "bold"}`}>
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+        <button
+          className="button is-danger"
+          onClick={() => dispatch(removeCar(car.id))}
+        >
+          Delete
+        </button>
+      </div>
     );
   });
 
--- cars/src/components/CarSearch.js
+++ cars/src/components/CarSearch.js
@@ -1,5 +1,23 @@
+import { useDispatch, useSelector } from "react-redux";
+import { changeSearchTerm } from "../store";
+
 const CarSearch = () => {
-  return <div>CarSearch</div>;
+  const dispatch = useDispatch();
+  const searchTerm = useSelector((state) => state.cars.searchTerm);
+
+  return (
+    <div className="list-header">
+      <h3 className="title is-3">My Cars</h3>
+      <div className="search field is-horizontal">
+        <label className="label">Search</label>
+        <input
+          className="input"
+          value={searchTerm}
+          onChange={(e) => dispatch(changeSearchTerm(e.target.value))}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default CarSearch;
